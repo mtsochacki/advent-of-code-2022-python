c84e2e3a5ff15d76e449c3da1458e603b7e13013
Mateusz Sochacki <mateusz.sochacki@icloud.com>
2022-12-06 10:55:46 +0100

    Prettify day06

--- src/day06.py
+++ src/day06.py
@@ -1,28 +1,22 @@
 def part1(buffer):
-    chunks = []
-    for i in range(0, len(buffer), 1):
-        chunks.append(buffer[i:i + 4])
-    start_of_packet = 0
-    for chunk in chunks:
-        set_of_chunk_characters = set(chunk)
-        print(set_of_chunk_characters)
-        if len(set_of_chunk_characters) == 4:
-            start_of_packet = buffer.find(chunk)
-            break
-    return start_of_packet + 4
+    return look_for_packet(buffer, 4)
+
 
 def part2(buffer):
+    return look_for_packet(buffer, 14)
+
+
+def look_for_packet(buffer, length):
     chunks = []
     for i in range(0, len(buffer), 1):
-        chunks.append(buffer[i:i + 14])
+        chunks.append(buffer[i:i + length])
     start_of_packet = 0
     for chunk in chunks:
         set_of_chunk_characters = set(chunk)
-        print(set_of_chunk_characters)
-        if len(set_of_chunk_characters) == 14:
+        if len(set_of_chunk_characters) == length:
             start_of_packet = buffer.find(chunk)
             break
-    return start_of_packet + 14
+    return start_of_packet + length
 
 
 if __name__ == '__main__':
